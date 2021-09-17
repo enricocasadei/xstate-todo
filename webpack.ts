f352df43f270d6c12a53261b988b59a2abcebe20
Enrico Casadei <enrico.casadei@truelayer.com>
2021-09-17 11:31:20 +0200

    ready to start

---
+++ webpack.ts
@@ -0,0 +1,54 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const config = {
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "../build",
+    historyApiFallback: true,
+    disableHostCheck: true,
+    port: 8090,
+    open: true,
+    hot: true,
+  },
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.join(__dirname, "build"),
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  target: "web",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: [require.resolve("react-refresh/babel")],
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(css)$/,
+        use: ["style-loader", "css-loader"],
+      },
+      { test: /\.(jpe?g|png|gif|ico|svg)$/i, use: "file?name=[name].[ext]" },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+};
+
+export default config;
